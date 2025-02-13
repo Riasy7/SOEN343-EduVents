234d97cefbd9eb6f00a938663c0c8f7043366e43
Gabriel Shufelt <gabshu2014@gmail.com>
2025-02-13 13:59:46 -0500

    Added Specs for User Models

---
+++ spec/factories/users.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :user do
+    first_name { "John" }
+    last_name { "Doe" }
+    username { Faker::Internet.unique.username }
+    email { Faker::Internet.unique.email }
+    password { "password" }
+    password_confirmation { "password" }
+
+    factory :attendee_speaker_user do
+      attendee_type { "speaker" }
+      username { "speaker" }
+      email { "speaker@edu-events.ca" }
+    end
+
+    factory :attendee_listener_user do
+      attendee_type { "listener" }
+      username { "listener" }
+      email { "listener@edu-events.ca" }
+    end
+  end
+end
--- spec/models/attendee_user_spec.rb
+++ spec/models/attendee_user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AttendeeUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when creating a attendee user" do
+    it "successfully creates a speaker attendee" do
+      speaker = create(:attendee_speaker_user)
+      expect(speaker).to be_valid
+    end
+
+    it "successfully creates a listener attendee" do
+      listener = create(:attendee_listener_user)
+      expect(listener).to be_valid
+    end
+  end
 end
