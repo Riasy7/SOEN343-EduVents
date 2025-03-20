4c6e2c40eaf465044aeea69762988bf078569a67
Vlad S <vlads@Vlads-MacBook-Air-2.local>
2025-03-20 17:44:06 -0400

    Display events in user feeds feature

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,10 +2,6 @@ class Organization < ApplicationRecord
     validates :name, presence: true
     validates :website, format: { with: /\Ahttps?:\/\/[\S]+\z/, message: "must be a valid URL" }, allow_blank: true
     validates :phone, format: { with: /\A\+?[0-9]{10,15}\z/, message: "must be a valid phone number" }, allow_blank: true
-    validates :locations, presence: { message: "must have at least one location" }
-    
-    has_many :users
-    has_many :locations, dependent: :destroy
 
-    accepts_nested_attributes_for :locations
+    has_many :users
 end
