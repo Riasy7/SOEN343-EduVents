101c5aa7c33fd23ed693927734f79e0308d4f720
Gabriel Shufelt <gabshu2014@gmail.com>
2025-03-21 15:18:47 -0400

    EventRegistration model + validations + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,6 @@ Organization.delete_all
 # Repopulate the db
 AdminUser.create!(username: "admin", password: "password", email: "admin@edu-events.ca", first_name: "John", last_name: "Doe")
 ExecutiveUser.create!(username: "executive", password: "password", email: "executive@edu-events.ca", first_name: "Jane", last_name: "Smith")
-OrganizerUser.create!(username: "organizer", password: "password", email: "organizer@edu-events.ca", first_name: "George", last_name: "Staples")
-AttendeeUser.create!(username: "speaker", password: "password", email: "speaker@edu-events.ca", first_name: "Marc", last_name: "Williams", attendee_type: "speaker")
-AttendeeUser.create!(username: "listener", password: "password", email: "listener@edu-events.ca", first_name: "Sarah", last_name: "Hull", attendee_type: "listener")
 
 # I have to nest the location inside the organization cause the location model has a belongs_to association with the organization model
 # however i can change the model validations itself but i did this instead
@@ -24,3 +21,9 @@ Organization.create!(
     { name: "Loyola Campus", address1: "7141 Sherbrooke St. W.", city: "Montreal", state: "QC", country: "CANADA", postal_code: "H4B 1R6" }
   ]
 )
+
+organizer = OrganizerUser.create!(username: "organizer", password: "password", email: "organizer@edu-events.ca", first_name: "George", last_name: "Staples")
+speaker = AttendeeUser.create!(username: "speaker", password: "password", email: "speaker@edu-events.ca", first_name: "Marc", last_name: "Williams", attendee_type: "speaker")
+listener = AttendeeUser.create!(username: "listener", password: "password", email: "listener@edu-events.ca", first_name: "Sarah", last_name: "Hull", attendee_type: "listener")
+event = Event.create!(name: "ConUHacks IX", event_type: "competition", location: "JMSB", organizer_id: organizer.id, price:0.00)
+EventRegistration.create!(event_id: event.id, attendee_id: listener.id)
---
+++ spec/factories/event_registrations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event_registration do
+    event { nil }
+    attendee { nil }
+    registered_as { "MyString" }
+  end
+end
