63d0994dee0c8a6e86e59a6c9c792cbc3638c2d9
Gabriel Shufelt <gabshu2014@gmail.com>
2025-03-22 07:31:06 -0400

    Venue seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@ AttendeeUser.delete_all
 Venue.delete_all
 Location.delete_all
 Organization.delete_all
+Location.delete_all
+Venue.delete_all
 
 # Repopulate the db
 AdminUser.create!(username: "admin", password: "password", email: "admin@edu-events.ca", first_name: "John", last_name: "Doe")
@@ -22,6 +24,7 @@ listener = AttendeeUser.create!(username: "listener", password: "password", emai
 organization = Organization.create!(name: "Concordia University", website: "https://www.concordia.ca", phone: "+5141234567")
 location = Location.create!(name: "SGW Campus", address1: "1455 De Maisonneuve Blvd. W.", address2: "Suite 203", city: "Montreal", state: "QC", country: "CANADA", postal_code: "H3G 1M8", organization_id: organization.id)
 venue = Venue.create!(name: "H - Sir George Williams University Alumni Auditorium (H-110)", max_capacity: 692, price_per_seat: 0.0, location_id: location.id)
+
 events = [
   { name: "Tech Innovators Summit", event_type: "conference" },
   { name: "AI & Machine Learning Workshop", event_type: "workshop" },
