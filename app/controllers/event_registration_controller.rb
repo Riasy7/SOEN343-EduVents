05fca8142cabaa63c720a1b29c4258d48157a38b
Gabriel Shufelt <gabshu2014@gmail.com>
2025-03-22 09:47:31 -0400

    Refactoring

--- app/controllers/event_registration_controller.rb
+++ app/controllers/event_registration_controller.rb
@@ -1,6 +1,12 @@
 class EventRegistrationController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event_registration, only: [:destroy, :show]
+  before_action :authorize_attendee!, only: [:destroy, :register]
+  before_action :check_owner!, only: [:destroy, :show]
+
   def destroy
     @event_registration = EventRegistration.find(params[:id])
+
     if @event_registration.destroy
       redirect_to events_path, notice: "Event registration was successfully canceled."
     else
@@ -12,12 +18,7 @@ class EventRegistrationController < ApplicationController
     @event = Event.find(params[:id])
     @user = current_user
 
-    unless @user.is_a?(AttendeeUser)
-      redirect_to events_path, alert: "Only attendees can register for events." and return
-    end
-
-    # if user is already registered for this event, return
-    if @user.event_registrations.where(event_id: @event.id).count > 0
+    if @user.registered_for_event?(@event.id)
       redirect_to events_path, alert: "You are already registered for this event." and return
     end
 
@@ -40,5 +41,24 @@ class EventRegistrationController < ApplicationController
 
   def event_registration_params
     params.require(:event_registration).permit(:event_id, :attendee_id)
+  end
+
+  def authorize_attendee!
+    unless current_user.is_a?(AttendeeUser)
+      redirect_to events_path, alert: "Only attendees can perform this action." and return
+    end
+  end
+
+  def check_owner!
+    unless @event_registration.attendee_id == current_user.id
+      redirect_to events_path, notice: "Only the owner of this event registration can perform this action."
+    end
+  end
+
+  def set_event_registration
+    @event_registration = EventRegistration.find_by(id: params[:id])
+    unless @event_registration
+      redirect_to events_path, alert: "Event registration not found."
+    end
   end
 end
--- app/models/attendee_user.rb
+++ app/models/attendee_user.rb
@@ -2,4 +2,12 @@ class AttendeeUser < User
   has_many :event_registrations, foreign_key: :attendee_id
 
   validates :attendee_type, presence: true, inclusion: { in: %(speaker listener) }
+
+  def get_registration_for_event(event_id)
+    event_registrations.find_by(event_id: event_id)&.id
+  end
+
+  def registered_for_event?(event_id)
+    EventRegistration.exists?(attendee_id: self.id, event_id:)
+  end
 end
