5cc757e8570db723fd83d467c05fd584fc90a47e
Ahmad Saadawi <138258066+Riasy7@users.noreply.github.com>
2025-04-08 11:57:44 -0400

    Messaging Feature (#90)
    * Finished User Story #13
    
    Implemented a chat room for unique events
    
    * Cleaned up UI for organizers
    
    * Refactored MessageMailer to NotificationService
    
    * Implemented the Notification Mailer Layout
    
    ---------
    
    Co-authored-by: Gabriel Shufelt <gabshu2014@gmail.com>

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,19 +1,26 @@
 class NotificationMailer < ApplicationMailer
-	def event_registration_confirmation(attendee, event_registration)
-		@attendee = attendee
-		@event_registration = event_registration
-		@event = event_registration.event
+  layout "notification_mailer"
 
-    	mail(
-      		to: @attendee.email,
-      		subject: "Registration Confirmation for #{@event.name}"
-    	)
-	end
+  def event_registration_confirmation(attendee, event_registration)
+    @attendee = attendee
+    @event_registration = event_registration
+    @event = event_registration.event
 
-	def event_reminder(user, event)
-		@user = user
-		@event = event
-		mail(to: @user.email, subject: "Reminder: #{@event.title}")
-	end
+      mail(
+          to: @attendee.email,
+          subject: "Registration Confirmation for #{@event.name}"
+      )
+  end
+
+  def event_reminder(user, event)
+    @user = user
+    @event = event
+    mail(to: @user.email, subject: "Reminder: #{@event.title}")
+  end
+
+  def new_event_message(user, message)
+    @message = message
+    @title = "New Message for #{@message.event.name}"
+    mail(to: user.email, subject: "New Message Notification")
+  end
 end
-  
