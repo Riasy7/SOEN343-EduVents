3d25e07a4a9497604f581d0b6fa2ba91dee4b917
Gabriel Shufelt <131308994+gabrielshufelt@users.noreply.github.com>
2025-04-08 14:15:08 -0400

    Clean up Events Management Views (#91)
    * WIP: local changes before pulling latest updates
    
    * Style event form and calendar, clean up assets
    
    * Style event form and calendar, clean up assets
    
    * Flatpickr Cleanup
    
    * fix: sugn out not working
    
    ---------
    
    Co-authored-by: Vladimir Shterenkiker <vlad.shteren@icloud.com>
    Co-authored-by: Idris Drouiche <157180914+IdrisDrouiche@users.noreply.github.com>
    Co-authored-by: IdrisDrouiche <idrisdrouiche@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// import "@hotwired/turbo-rails";
+// import "controllers";
+// import Rails from "@rails/ujs";
+// Rails.start();
+
+//Flatpicker
 import "@hotwired/turbo-rails";
 import "controllers";
-import "@hotwired/turbo-rails";
-import Rails from "@rails/ujs";
-Rails.start();
+import flatpickr from "flatpickr";
+
+document.addEventListener("turbo:load", () => {
+  flatpickr(".datetime-picker.start-time", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today",
+    defaultHour: 9,
+    onChange: function (selectedDates) {
+      const endInput = document.querySelector(".datetime-picker.end-time");
+      if (endInput && endInput._flatpickr && selectedDates[0]) {
+        const oneHourLater = new Date(selectedDates[0].getTime() + 60 * 60 * 1000);
+        endInput._flatpickr.setDate(oneHourLater, true);
+      }
+    }
+  });
+
+  flatpickr(".datetime-picker.end-time", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today"
+  });
+});
