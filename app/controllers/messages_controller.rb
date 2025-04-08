efccedce867c692748f324f123771878bee5a19b
Gabriel Shufelt <gabshu2014@gmail.com>
2025-04-07 23:34:31 -0400

    Refactored MessageMailer to NotificationService

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,41 +1,41 @@
 class MessagesController < ApplicationController
     before_action :authenticate_user!
     before_action :set_event
-    before_action :validate_permissions, only: [:index, :create]
-  
+    before_action :validate_permissions, only: [ :index, :create ]
+
     def index
       @messages = Message.where(event: @event).order(:created_at)
     end
-  
+
     def create
         @message = Message.new(message_params)
         @message.sender = current_user
         @message.event = @event
-      
+
         if current_user.is_a?(AttendeeUser)
           @message.receiver = @event.organizer
         elsif current_user.is_a?(OrganizerUser)
           @message.receiver = User.find(params[:message][:receiver_id])
         end
-      
+
         if @message.save
-          MessageMailer.new_message_notification(@message).deliver_later
+          NotificationService.call(current_user, :new_event_message, @message)
           redirect_to event_messages_path(@event), notice: "Message sent successfully."
         else
           redirect_to event_messages_path(@event), alert: "Failed to send message."
         end
     end
-  
+
     private
-  
+
     def set_event
       @event = Event.find(params[:event_id])
     end
-  
+
     def message_params
         params.require(:message).permit(:content, :receiver_id)
     end
-  
+
     def validate_permissions
       if current_user.is_a?(AttendeeUser)
         unless current_user.registered_for_event?(@event.id)
@@ -49,4 +49,4 @@ class MessagesController < ApplicationController
         redirect_to root_path, alert: "You are not authorized to access this page."
       end
     end
-  end
+end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,19 +1,23 @@
 class NotificationMailer < ApplicationMailer
-	def event_registration_confirmation(attendee, event_registration)
-		@attendee = attendee
-		@event_registration = event_registration
-		@event = event_registration.event
+  def event_registration_confirmation(attendee, event_registration)
+    @attendee = attendee
+    @event_registration = event_registration
+    @event = event_registration.event
 
-    	mail(
-      		to: @attendee.email,
-      		subject: "Registration Confirmation for #{@event.name}"
-    	)
-	end
+      mail(
+          to: @attendee.email,
+          subject: "Registration Confirmation for #{@event.name}"
+      )
+  end
 
-	def event_reminder(user, event)
-		@user = user
-		@event = event
-		mail(to: @user.email, subject: "Reminder: #{@event.title}")
-	end
+  def event_reminder(user, event)
+    @user = user
+    @event = event
+    mail(to: @user.email, subject: "Reminder: #{@event.title}")
+  end
+
+  def new_event_message(user, message)
+    @message = message
+    mail(to: user.email, subject: "New Message for #{@message.event.name}")
+  end
 end
-  
